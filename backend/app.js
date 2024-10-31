7ccb4640b9d98ed3f98e1c36cc5e23ff6c1484c8
Loup13250 <loupferri@gmail.com>
2024-10-31 02:55:22 +0100

    nodemon express mongoose

--- backend/app.js
+++ backend/app.js
@@ -12,15 +12,16 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  console.log("Incoming request");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // accéder à l'API depuis n'importe quelle origine ( '*' )
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
+  ); //ajoute les headers mentionnés aux requêtes envoyées vers API (Origin , X-Requested-With , etc.)
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  ); //envoyer des requêtes avec les méthodes GET ,POST , PUT etc..
   next();
 });
 
