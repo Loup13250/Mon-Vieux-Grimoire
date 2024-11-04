aebf654bbad85446a4d317e7bd0731d77614f6d2
Loup13250 <loupferri@gmail.com>
2024-11-04 03:59:10 +0100

    site ok

--- backend/app.js
+++ backend/app.js
@@ -12,16 +12,13 @@ app.use(express.json());
 // afficher les images directement sur mon site
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-// Check if dotenv was loaded properly
+// Check if dotenv bien lancé
 if (!process.env.MONGO_URI) {
   console.log("Erreur lors du chargement des variables d'environnement.");
   process.exit(1);
 }
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
